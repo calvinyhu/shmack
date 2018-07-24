0fdf006395c6976cde887f5e965188e6f0517d79
Calvin Hu <cahu@ucdavis.edu>
2018-07-23 22:37:56 -0700

    Added Layout and Auxiliary

---
+++ src/hoc/Auxiliary/Auxiliary.js
@@ -0,0 +1,4 @@
+// @auxiliary allows adjacent JSX to be next to each other.
+const auxiliary = (props) => props.children
+
+export default auxiliary
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import Auxiliary from '../Auxiliary/Auxiliary'
+
+// @Layout holds the different pages or views of the app and will keep either a 
+// persistent toolbar or sidedrawer for user navigation.
+class Layout extends Component {
+    render() {
+        return (
+            <Auxiliary>
+                {/* Toolbar */}
+                {/* Sidedrawer */}
+                <main>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        )
+    }
+}
+
+export default Layout
