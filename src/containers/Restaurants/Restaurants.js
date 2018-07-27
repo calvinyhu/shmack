cc5643d7b862bccbe70e7859efcf70c9242f0fc2
Calvin Hu <cahu@ucdavis.edu>
2018-07-26 17:14:39 -0700

    Added basic grid with placeholder image.

--- src/containers/Restaurants/Restaurants.js
+++ src/containers/Restaurants/Restaurants.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 
+import classes from './Restaurants.css'
+
 class Restaurants extends Component {
+    state = {
+        restaurants: {
+            count: 50
+        }
+    }
+
     render() {
+        let restaurants = [];
+        for (let i = 1; i <= this.state.restaurants.count; i = i + 1) {
+            restaurants.push(
+                <div key={i}className={classes.RestaurantsGridItem}>Restaurant Image {i}</div>
+            );
+        }
+
         return (
-            <div>
-                <ul>
-                    <li>Restaurant 1</li>
-                    <li>Restaurant 2</li>
-                    <li>Restaurant 3</li>
-                </ul>
+            <div className={classes.Restaurants}>
+                <div className={classes.RestaurantsGrid}>
+                    {restaurants}
+                </div>
             </div>
         )
     }
