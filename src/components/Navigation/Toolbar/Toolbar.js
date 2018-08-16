f3289a097aaa1ed82d28b4de5a0a0800e897f447
Calvin Hu <cahu@ucdavis.edu>
2018-08-16 14:04:05 -0700

    Added search bars.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,6 +8,10 @@ const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
             <p><strong>3FINITY</strong></p>
+            <div className={classes.ToolbarSearch} >
+                <input type='text' placeholder='Search' />
+                <input type='text' placeholder='Location' />
+            </div>
             <SideDrawerToggle toggleSideDrawer={props.toggleSideDrawer} />
             <nav>
                 <NavigationItems />
