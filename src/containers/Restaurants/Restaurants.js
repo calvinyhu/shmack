114360781ca885e09136c4951c6235a139362bde
Calvin Hu <cahu@ucdavis.edu>
2018-08-17 19:26:26 -0700

    Adding yelp querying

--- src/containers/Restaurants/Restaurants.js
+++ src/containers/Restaurants/Restaurants.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import classes from './Restaurants.css'
 
 class Restaurants extends Component {
     state = {
+        food: '',
+        location: '',
         restaurants: {
             count: 50
         }
+    }
+
+    foodChangeHandler = (event) => {
+        this.setState({ food: event.target.value })
+    }
+
+    locationChangeHandler = (event) => {
+        this.setState({ location: event.target.value })
+    }
+
+    searchHandler = (event) => {
+        const cors = 'https://cors-anywhere.herokuapp.com/'
+        const query = `https://api.yelp.com/v3/businesses/search?term=${this.state.food}&location=${this.state.location}`
+        const apiKey = 'Xj6GzMkzzzCUC5j_494Gyv40BfT0xBzaGIxvPGwGkiiUp1qFYEyNEG9Xnfxr9t-v33TTWPn-kGypQy_ZAN1tScZk7jDUYKHLObbjDdR0TqtEHQqWAwUtlHTkzIB3W3Yx'
+        const config = {
+            headers: {
+                Authorization: 'Bearer: ' + apiKey
+            }
+        }
+
+        axios.get(cors + query, config)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+        event.preventDefault();
     }
 
     render() {
@@ -23,9 +55,9 @@ class Restaurants extends Component {
                     {restaurants}
                 </div>
                 <div className={classes.SearchBar}>
-                    <input type='text' placeholder='Search' />
-                    <input type='text' placeholder='Location' />
-                    <button type='text' className={classes.Go}>Go</button>
+                    <input type='text' placeholder='Food' onChange={this.foodChangeHandler}/>
+                    <input type='text' placeholder='Location' onChange={this.locationChangeHandler}/>
+                    <button type='text' className={classes.SearchButton} onClick={this.searchHandler}>Go</button>
                 </div>
             </div>
         )
