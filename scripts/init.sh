cc48246f7de938aa7090dbe71c38806dc563af0e
Calvin Hu <ycalvinhu@gmail.com>
2018-08-22 15:49:33 -0700

    Added googleApiKey to init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,8 @@
 
 # Export necessary javascript variables to the secrets.js if the file does not exist.
 if [ ! -f ./src/secrets.js ]; then
-    echo "export const yelpApiKey = 'YELP_API_KEY'" > src/secrets.js
+    echo "export const googleApiKey = 'GOOGLE_API_KEY'" >> src/secrets.js
+    echo "export const yelpApiKey = 'YELP_API_KEY'" >> src/secrets.js
 else
-    echo "./src/secrets.js exists! Continuing with npm start..."
+    echo "[./src/secrets.js] already exists! Continuing with npm start..."
 fi
