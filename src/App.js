ccd66a18cac48744c4c5df20b6899201e8d0ff79
Calvin Hu <ycalvinhu@gmail.com>
2018-08-22 11:54:42 -0700

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { connect } from 'react-redux'
 
 import * as actions from './store/actions/authActions'
 import Layout from './hoc/Layout/Layout'
-import Restaurants from './containers/Restaurants/Restaurants'
 import About from './containers/About/About'
 import Auth from './containers/Auth/Auth'
 import LogOut from './containers/Auth/LogOut/LogOut'
+import Restaurants from './containers/Restaurants/Restaurants'
 
 const mapStateToProps = (state) => {
     return {
@@ -27,21 +27,23 @@ class App extends Component {
     }
 
     render() {
-        let routes = (
-            <Switch>
-                <Route exact path='/about' component={About} />
-                <Route exact path='/auth/signup' component={Auth} />
-                <Route exact path='/auth/login' component={Auth} />
-                <Route exact path='/' component={Restaurants} />
-                <Redirect to='/' />
-            </Switch>
-        )
+        let routes = null
 
         if (this.props.isAuth) {
             routes = (
                 <Switch>
                     <Route exact path='/about' component={About} />
                     <Route exact path='/logout' component={LogOut} />
+                    <Route exact path='/' component={Restaurants} />
+                    <Redirect to='/' />
+                </Switch>
+            )
+        } else {
+            routes = (
+                <Switch>
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/auth/signup' component={Auth} />
+                    <Route exact path='/auth/login' component={Auth} />
                     <Route exact path='/' component={Restaurants} />
                     <Redirect to='/' />
                 </Switch>
